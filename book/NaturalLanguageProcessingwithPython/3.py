fcb41e3357e12c481e2cd71361b9fd5b98351e74
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-09-01 21:08:27 +0900

    Execute ch3

--- book/NaturalLanguageProcessingwithPython/3.py
+++ book/NaturalLanguageProcessingwithPython/3.py
@@ -33,7 +33,7 @@ import urllib
 #text = nltk.Text(tokens)
 #print(text.concordance('gene'))
 
-import feedparser
+#import feedparser
 #llog  = feedparser.parse("http://languagelog.ldc.upenn.edu/nll/?feed=atom")
 #print(llog['feed']['title'])
 #print(len(llog.enties))
@@ -69,65 +69,120 @@ import os
 #beatles = ['john', 'paul', 'george', 'ringo']
 ## print(query + beatles) # Error
 
-monty = 'Monty Python'
-print(monty)
-circus = "Monty Python's Flying Circus"
-couplet = "Shall I compare thee to a Summer's day?"\
-          "Thou are more lovely and more temperate:"
-print(couplet)
-couplet = ("Rough winds do shake the darling buds of May,"\
-           "And Summer's lease hath all to short a date:")
-print(couplet)
-couplet = """Shall I compare thee to a Summer's day?
-Thou are more lovely and more temperate:"""
-print(couplet)
-print('very'+'very'+'very')
-print('very'*3)
-a = [1,2,3,4,5,6,7,6,5,4,3,2,1]
-b = [' '*2*(7-i)+'very'*i for i in a]
-for line in b:
-    print(line)
-from nltk.corpus import gutenberg
-raw = gutenberg.raw('melville-moby_dick.txt')
-fdist = nltk.FreqDist(ch.lower() for ch in raw if ch.isalpha())
-print(fdist.keys())
-import codecs
-path = nltk.data.find('corpora/unicode_samples/polish-lat2.txt')
-f = codecs.open(path, encoding='latin2')
-for line in f:
-    line = line.strip()
-    print(line.encode('unicode_escape'))
-print(ord('a'))
-print(u'\u0061')
-nacute = u'\u0144'
-print(nacute)
-nacute_utf = nacute.encode('utf8')
-print(repr(nacute_utf))
-import unicodedata
-lines = codecs.open(path, encoding='latin2').readlines()
-line = lines[2]
-print(line.encode('unicode_escape'))
-for c in line:
-    if ord(c) > 127:
-        print('%r U+%04x %s' % (c.encode('utf8'), ord(c), unicodedata.name(c)))
-print(line.find(u'zosta\u0142y'))
-line = line.lower()
-print(line.encode('unicode_escape'))
-import re
-m = re.search(u'\u015b\w*', line)
-print(m.group())
-print(nltk.word_tokenize(line))
-wordlist = [w for w in nltk.corpus.words.words('en') if w.islower()]
-print([w for w in wordlist if re.search('en', w)])
-print([w for w in wordlist if re.search('^..j..t..$', w)])
-print([w for w in wordlist if re.search('^[ghi][mno][jlk][def]$', w)])
-chat_words = sorted(set(w for w in nltk.corpus.nps_chat.words()))
-print([w for w in chat_words if re.search('^m+i+n+e+$', w)])
-print([w for w in chat_words if chat_words if re.search('^[ha]+$', w)])
-wsj = sorted(set(nltk.corpus.treebank.words()))
-print([w for w in wsj if re.search('^[0-9]+\.[0-9]+$', w)])
-print([w for w in wsj if re.search('^[A-Z]+\$$', w)])
-print([w for w in wsj if re.search('^[0-9]{4}$', w)])
-print([w for w in wsj if re.search('^[0-9]+-[a-z]{3,5}$', w)])
-print([w for w in wsj if re.search('^[a-z]{5,}-[a-z]{,6}$', w)])
-print([w for w in wsj if re.search('(ed|ing)$', w)])
+#monty = 'Monty Python'
+#print(monty)
+#circus = "Monty Python's Flying Circus"
+#couplet = "Shall I compare thee to a Summer's day?"\
+#          "Thou are more lovely and more temperate:"
+#print(couplet)
+#couplet = ("Rough winds do shake the darling buds of May,"\
+#           "And Summer's lease hath all to short a date:")
+#print(couplet)
+#couplet = """Shall I compare thee to a Summer's day?
+#Thou are more lovely and more temperate:"""
+#print(couplet)
+#print('very'+'very'+'very')
+#print('very'*3)
+#a = [1,2,3,4,5,6,7,6,5,4,3,2,1]
+#b = [' '*2*(7-i)+'very'*i for i in a]
+#for line in b:
+#    print(line)
+#from nltk.corpus import gutenberg
+#raw = gutenberg.raw('melville-moby_dick.txt')
+#fdist = nltk.FreqDist(ch.lower() for ch in raw if ch.isalpha())
+#print(fdist.keys())
+#import codecs
+#path = nltk.data.find('corpora/unicode_samples/polish-lat2.txt')
+#f = codecs.open(path, encoding='latin2')
+#for line in f:
+#    line = line.strip()
+#    print(line.encode('unicode_escape'))
+#print(ord('a'))
+#print(u'\u0061')
+#nacute = u'\u0144'
+#print(nacute)
+#nacute_utf = nacute.encode('utf8')
+#print(repr(nacute_utf))
+#import unicodedata
+#lines = codecs.open(path, encoding='latin2').readlines()
+#line = lines[2]
+#print(line.encode('unicode_escape'))
+#for c in line:
+#    if ord(c) > 127:
+#        print('%r U+%04x %s' % (c.encode('utf8'), ord(c), unicodedata.name(c)))
+#print(line.find(u'zosta\u0142y'))
+#line = line.lower()
+#print(line.encode('unicode_escape'))
+#import re
+#m = re.search(u'\u015b\w*', line)
+#print(m.group())
+#print(nltk.word_tokenize(line))
+#wordlist = [w for w in nltk.corpus.words.words('en') if w.islower()]
+#print([w for w in wordlist if re.search('en', w)])
+#print([w for w in wordlist if re.search('^..j..t..$', w)])
+#print([w for w in wordlist if re.search('^[ghi][mno][jlk][def]$', w)])
+#chat_words = sorted(set(w for w in nltk.corpus.nps_chat.words()))
+#print([w for w in chat_words if re.search('^m+i+n+e+$', w)])
+#print([w for w in chat_words if chat_words if re.search('^[ha]+$', w)])
+#wsj = sorted(set(nltk.corpus.treebank.words()))
+#print([w for w in wsj if re.search('^[0-9]+\.[0-9]+$', w)])
+#print([w for w in wsj if re.search('^[A-Z]+\$$', w)])
+#print([w for w in wsj if re.search('^[0-9]{4}$', w)])
+#print([w for w in wsj if re.search('^[0-9]+-[a-z]{3,5}$', w)])
+#print([w for w in wsj if re.search('^[a-z]{5,}-[a-z]{,6}$', w)])
+#print([w for w in wsj if re.search('(ed|ing)$', w)])
+
+#word = 'supercalifragilisticexpialidocious'
+#print(re.findall(r'[aeiou]', word))
+#print(len(re.findall(r'[aeiou]', word)))
+#wsj = sorted(set(nltk.corpus.treebank.words()))
+#fd = nltk.FreqDist(vs for word in wsj for vs in re.findall(r'[aeiou]{2,}', word))
+#print(fd.items())
+#regexp = r'^[AEIOUaeiou]+|[AEIOUaeiou]+$|[^AEIOUaeiou]'
+#def compress(word):
+#    pieces = re.findall(regexp, word)
+#    return ''.join(pieces)
+#english_udhr = nltk.corpus.udhr.words('English-Latin1')
+#print(nltk.tokenwrap(compress(w) for w in english_udhr[:75]))
+#rotokas_words = nltk.corpus.toolbox.words('rotokas.dic')
+#cvs = [cv for w in rotokas_words for cv in re.findall(r'[ptksvr][aeiou]', w)]
+#cfd = nltk.ConditionalFreqDist(cvs)
+#cfd.tabulate()
+#cv_word_pairs = [(cv, w) for w in rotokas_words for cv in re.findall(r'[ptksvr][aeiou]', w)]
+#cv_index = nltk.Index(cv_word_pairs)
+#print(cv_index['su'])
+#print(cv_index['po'])
+
+#def stem(word):
+#    for suffix in ['ing', 'ly', 'ed', 'ious', 'ies', 'ive', 'es', 's', 'ment']:
+#        if word.endswith(suffix):
+#            return word[:-len(suffix)]
+#    return word
+#print(re.findall(r'^.*(ing|ly|ed|ious|ies|ive|es|s|ment)$' 'processing'))
+#print(re.findall(r'^(.*)(ing|ly|ed|ious|ies|ive|es|s|ment)$' 'processing'))
+#print(re.findall(r'^(.*)(ing|ly|ed|ious|ies|ive|es|s|ment)$' 'processes'))
+#print(re.findall(r'^(.*?)(ing|ly|ed|ious|ies|ive|es|s|ment)$' 'processes'))
+#print(re.findall(r'^(.*?)(ing|ly|ed|ious|ies|ive|es|s|ment)$' 'language'))
+#def stem(word):
+#    regexp = r'^(.*?)(ing|ly|ed|ious|ies|ive|es|s|ment)$'
+#    stem, suffix = re.findall(regexp, word)[0]
+#    return stem
+#raw = """DENNIS: Lsten, strange women lying in ponds distributing swords
+#is no basis for a system of government. Supreme executive power derives from
+#a mandate from the masses, not from some farcical aquatic ceremony."""
+#tokens = nltk.word_tokenize(raw)
+#print([stem(t) for t in tokens])
+#from nltk.corpus import gutenberg, nps_chat
+#moby = nltk.Text(gutenberg.words('melville-moby_dick.txt'))
+#print(moby.findall(r"<a>(<.*>)<man>"))
+#chat = nltk.Text(nps_chat.words())
+#chat.findall(r"<il.*>{3,}")
+#from nltk.corpus import brown
+#hobbies_learned = nltk.Text(brown.words(categories=['hobbies', 'learned']))
+#hobbies_learned.findall(r"<\w*><and><other><\w*s>")
+#tokens = nltk.word_tokenize(raw)
+
+#P113
+raw = """DENNIS: Lsten, strange women lying in ponds distributing swords
+is no basis for a system of government. Supreme executive power derives from
+a mandate from the masses, not from some farcical aquatic ceremony."""
