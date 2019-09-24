0639807f48b05a76b89f59779421f18494575c3c
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-09-24 10:38:03 +0900

    Fin ch5

--- book/NaturalLanguageProcessingwithPython/5.py
+++ book/NaturalLanguageProcessingwithPython/5.py
@@ -169,4 +169,107 @@
 #    pos2[value].append(key)
 #print(pos2['ADJ'])
 
-# P214 Automation Tag
+#import nltk
+#from nltk.corpus import brown
+#brown_tagged_sents = brown.tagged_sents(categories='news')
+#brown_sents = brown.sents(categories='news')
+#tags = [tag for (word, tag) in brown.tagged_words(categories='news')]
+#print(nltk.FreqDist(tags).max())
+#raw = 'I do not like green eggs and ham, I do not like them Sam I am!'
+#tokens = nltk.word_tokenize(raw)
+#default_tagger = nltk.DefaultTagger('NN')
+#print(default_tagger.tag(tokens))
+#print(default_tagger.evaluate(brown_tagged_sents))
+#patterns = [
+#      (r'.*ing$',               'VBG'),
+#      (r'.*ed$',                'VBD'),
+#      (r'.*es$',                'VBZ'),
+#      (r'.*ould$',              'MD'),
+#      (r'.*\'s$',               'NN$'),
+#      (r'.*s$',                 'MNS'),
+#      (r'.^-?[0-9]+(.[0-9]+)?$','CD'),
+#      (r'.*',                   'NN')
+#    ]
+#regexp_tagger = nltk.RegexpTagger(patterns)
+#print(regexp_tagger.tag(brown_sents[3]))
+#print(regexp_tagger.evaluate(brown_tagged_sents))
+#fd = nltk.FreqDist(brown.words(categories='news'))
+#cfd = nltk.ConditionalFreqDist(brown.tagged_words(categories='news'))
+#most_freq_words = list(fd.keys())[:100]
+#likely_tags = dict((word, cfd[word].max()) for word in most_freq_words)
+#baseline_tagger = nltk.UnigramTagger(model=likely_tags)
+#print(baseline_tagger.evaluate(brown_tagged_sents))
+#sent = brown.sents(categories='news')[3]
+#print(baseline_tagger.tag(sent))
+#baseline_tagger = nltk.UnigramTagger(model=likely_tags, backoff=nltk.DefaultTagger('NN'))
+#def performance(cfd, wordlist):
+#    lt = dict((word, cfd[word].max()) for word in wordlist)
+#    baseline_tagger = nltk.UnigramTagger(model=lt, backoff=nltk.DefaultTagger('NN'))
+#    return baseline_tagger.evaluate(brown.tagged_sents(categories='news'))
+#def display():
+#    import pylab
+#    words_by_freq = list(nltk.FreqDist(brown.words(categories='news')))
+#    cfd = nltk.ConditionalFreqDist(brown.tagged_words(categories='news'))
+#    sizes = 2 ** pylab.arange(15)
+#    perfs = [performance(cfd, words_by_freq[:size]) for size in sizes]
+#    pylab.plot(sizes, perfs, '-bo')
+#    pylab.title('Lookup Tagger Performance with Varying Model Size')
+#    pylab.xlabel('Model Size')
+#    pylab.ylabel('Performance')
+#    pylab.show()
+#display()
+
+#import nltk
+#from nltk.corpus import brown
+#brown_tagged_sents = brown.tagged_sents(categories='news')
+#brown_sents = brown.sents(categories='news')
+#unigram_tagger = nltk.UnigramTagger(brown_tagged_sents)
+#print(unigram_tagger.tag(brown_sents[2007]))
+#print(unigram_tagger.evaluate(brown_tagged_sents))
+#size = int(len(brown_tagged_sents) * 0.9)
+#print(size)
+#train_sents = brown_tagged_sents[:size]
+#test_sents = brown_tagged_sents[size:]
+#unigram_tagger = nltk.UnigramTagger(train_sents)
+#print(unigram_tagger.evaluate(test_sents))
+#bigram_tagger = nltk.BigramTagger(train_sents)
+#print(bigram_tagger.tag(brown_sents[2007]))
+#unseen_sent = brown_sents[4203]
+#print(bigram_tagger.tag(unseen_sent))
+#print(bigram_tagger.evaluate(test_sents))
+#t0 = nltk.DefaultTagger('NN')
+#t1 = nltk.UnigramTagger(train_sents, backoff=t0)
+#t2 = nltk.BigramTagger(train_sents, backoff=t1)
+#print(t2.evaluate(test_sents))
+#from pickle import dump
+#output = open('t2.pkl', 'wb')
+#output.close()
+#from pickle import load
+#input = open('t2.pkl', 'rb')
+#tagger = load(input)
+#input.close()
+#text = """The board's action shows what free enterprise
+#is up against in our complex maze of regulaory laws ."""
+#tokens = text.split()
+#print(tagger.tag(tokens))
+#cfd = nltk.ConditionalFreqDist(
+#            ((x[1], y[1], z[0]), z[1])
+#            for sent in brown_tagged_sents
+#            for x, y, z in nltk.trigrams(sent)
+#)
+#ambiguous_contexts = [c for c in cfd.conditions() if len(cfd[c]) > 1]
+#print(sum(cfd[c].N() for c in ambiguous_contexts) / cfd.N())
+#test_tags = [tag for sent in brown.sents(categories='editorial') for (word, tag) in t2.tag(sent)]
+#gold_tags = [tag for (word, tag) in brown.tagged_words(categories='editorial')]
+#print(nltk.ConfusionMatrix(gold, test))
+#brown_tagged_sents = brown.tagged_sents(categories='news')
+#brown_sents = brown.sents(categories='news')
+#size = int(len(brown_tagged_sents)*0.9)
+#train_sents = brown_tagged_sents[:size]
+#test_sents = brown_tagged_sents[size:]
+#t0 = nltk.DefaultTagger('NN')
+#t1 = nltk.UnigramTagger(train_sents, backoff=t0)
+#t2 = nltk.BigramTagger(train_sents, backoff=t1)
+#print(t2.evaluate(test_sents))
+#nltk.tag.brill.demo()
+#print(open("errors.out").read())
