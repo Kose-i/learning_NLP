df32421fb658a3512385e0cf0d722985d1dd65b9
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-10-07 16:48:37 +0900

    Fin ch12

--- book/NaturalLanguageProcessingwithPython/12.py
+++ book/NaturalLanguageProcessingwithPython/12.py
@@ -257,135 +257,198 @@
 #S = [u'30年も前に言語と画像を研究していた。'.encode('euc-jp')]
 #print(cJuman.parse_opt(S, cJuman.SKIP_NO_RESULT).decode('euc-jp'))
 
-import nltk
-from nltk import CFG
-jpcfg1 = nltk.CFG.fromstring("""
-S    -> PP VP
-PP   -> NP P
-VP   -> PP VP
-VP   -> V TENS
-NP   -> NP 'の' NP
-NP   -> NP 'と' NP
-NP   -> N
-N    -> '先生' | '自転車' | '学校' | '僕'
-P    -> 'は' | 'が' | 'で' | 'に'
-V    -> '行k' | '殴r' | '見'
-TENS -> 'ru' | 'ita'
-""")
-sent = "先生 は 自転車 で 学校 に 行k ita".split(' ')
-parser = nltk.ChartParser(jpcfg1)
-for tree in next(parser.parse(sent)):
-    print(tree)
-sent = "先生 は 自転車 で 学校 に 行k ita".split(' ')
-parser = nltk.ChartParser(jpcfg1)
-for tree in next(parser.parse(sent)):
-    print(tree)
-genpaku = ChasenCorpusReader('/path/to/jeita/corpus/', 'g0014.chasen', encoding='utf-8')
-print('/'.join(genpaku.words()[80:120]))
-grammer = u"""
-JIRITSU: {<形容詞-自立>|<名詞.*>|<未知語>|<動詞-自立>|<記号-一般>|<副詞.*>}
-FUZOKU:  {<助動詞>|<助詞.*>|<動詞-接尾>|<動詞-非自立>|<名詞-非自立>}
-CHUNK:   {<JIRITSU>+<FUZOKU>*}
-"""
-cp = nltk.RegexpParser(grammer)
-print(cp.parse([(x, '-'.join(y[2].split('-')[0:2])) for x,y in genpaku.tagged_words()[80:120]]))
-import CaboCha, re
-cabocha = CaboCha.Parser('--charset=UTF8')
-sent = u"太郎はこの本を二郎を見た女性に渡した。".encode('utf-8')
-print(cabocha.perseToString(sent))
-tree = cabocha.parse(sent)
-print(tree.toString(CaboCha.FORMAT_LATTICE))
-def cabocha2depgraph(t):
-    dg = DependencyGraph()
-    i = 0
-    for line in t.splitlines():
-        if line.startswith("*"):
-            # start of bunsetsu
-            cells = line.strip().split(" ", 3)
-            m = re.match(r"([\-0-9]*)([ADIP])", cells[2])
-            node = dg.nodelist[i]
-            node.update(
-              {'address': i,
-               'rel': m.group(2), # dep_type
-               'word': [],
-               'tag': []
-              })
-            dep_parent = int(m.group(1))
-            while len(dg.nodelist) < i+1 or len(dg.nodelist) < dep_parent+1:
-                dg.nodelist.append({'word':[], 'deps':[], 'tag':[]})
-            if dep_parent == -1:
-                dg.root = node
-            else:
-                dg.nodelist[dep_parent]['deps'].append(i)
-            i += 1
-        elif not line.startswith("EOS"):
-            # normal morph
-            cells = line.strip().split("\t")
-            morph = (cells[0], tuple(cells[1].split(',')))
-            dg.nodelist[i-1]['word'].append(morph[0])
-            dg.nodelist[i-1]['tag'].append(morph[1])
-    return dg
-def reset_deps(dg):
-    for node in dg.nodelist:
-        node['deps'] = []
-    dg.root = dg.nodelist[-1]
-def set_head_form(dg):
-    for node in dg.nodelist:
-        tags = node['tag']
-        num_morphs = len(tags)
-        # extract bhead and bform
-        bhead = -1
-        bform = -1
-        for i in xrange(num_morphs - 1, -1, -1):
-            if tags[i][0] == u"記号":
-                continue
-            else:
-                if bform == -1:
-                    bform = i
-                if not (tags[i][0] == u"" or (tags[i][0] == u"" and tags[i][1] == u"") or tags[i][0] == ""):
-                    if bhead == -1:
-                        bhead = i
-        node['bhead'] = bhead
-        node['bform'] = bform
-NEXT_NODE = 1
-NEXT_VERB_NODE = 2
-NEXT_NOUN_NODE = 3
-def get_dep_type(node):
-    bform_tag = node['tag'][node['bform']]
-    if bform_tag[0] == u"助詞" and bform_tag[1] == u"格助詞":
-        return NEXT_VERB_NODE
-    elif bform_tag[0] == u"助動詞" and bform_tag[1] == u"夕":
-        return NEXT_NOUN_NODE
-    else:
-        return NEXT_NODE
-def analyze_dependency(dg):
-    num_nodes = len(dg.nodelist)
-    for i in xrange(num_nodes-1, 0, -1):
-        node = dg.nodelist[i]
-        if i == num_nodes-1:
-            #last node->to_node=0
-            to_node = 0
-        elif i == num_nodes-2:
-            #one from the last node -> to_node = num_nodes -1
-            to_node = num_nodes -1
-        else:
-            #other nodes
-            dep_type = get_dep_type(node)
-            if dep_type == NEXT_NODE:
-                to_node = i+1
-            elif dep_type == NEXT_VERB_NODE or dep_type == NEXT_NOUN_NODE:
-                for j in xrange(i+1, num_nodes):
-                    node_j = dg.nodelist[j]
-                    node_j_headtag = node_j['tag'][node_j['bhead']]
-                    if (node_j['closed'] == False and (dep_type == NEXT_VERB_NODE and node_j_headtag[0] == u"動詞") or 
-                                                      (dep_type == NEXT_NOUN_NODE and node_j_headtag[0] == u"名詞") and node_j_headtag[1] != u"形容動詞語幹"):
-                                                      to_node = j
-                                                      break
-                                                      node['head'] = to_node
-                                                      dg.nodelist[to_node]['deps'].append(i)
-                                                      for j in xrange(i+1, to_node):
-                                                          dg.nodelist[j]['closed'] = True
+#import nltk
+#from nltk import CFG
+#jpcfg1 = nltk.CFG.fromstring("""
+#S    -> PP VP
+#PP   -> NP P
+#VP   -> PP VP
+#VP   -> V TENS
+#NP   -> NP 'の' NP
+#NP   -> NP 'と' NP
+#NP   -> N
+#N    -> '先生' | '自転車' | '学校' | '僕'
+#P    -> 'は' | 'が' | 'で' | 'に'
+#V    -> '行k' | '殴r' | '見'
+#TENS -> 'ru' | 'ita'
+#""")
+#sent = "先生 は 自転車 で 学校 に 行k ita".split(' ')
+#parser = nltk.ChartParser(jpcfg1)
+#for tree in next(parser.parse(sent)):
+#    print(tree)
+#sent = "先生 は 自転車 で 学校 に 行k ita".split(' ')
+#parser = nltk.ChartParser(jpcfg1)
+#for tree in next(parser.parse(sent)):
+#    print(tree)
+#genpaku = ChasenCorpusReader('/path/to/jeita/corpus/', 'g0014.chasen', encoding='utf-8')
+#print('/'.join(genpaku.words()[80:120]))
+#grammer = u"""
+#JIRITSU: {<形容詞-自立>|<名詞.*>|<未知語>|<動詞-自立>|<記号-一般>|<副詞.*>}
+#FUZOKU:  {<助動詞>|<助詞.*>|<動詞-接尾>|<動詞-非自立>|<名詞-非自立>}
+#CHUNK:   {<JIRITSU>+<FUZOKU>*}
+#"""
+#cp = nltk.RegexpParser(grammer)
+#print(cp.parse([(x, '-'.join(y[2].split('-')[0:2])) for x,y in genpaku.tagged_words()[80:120]]))
+#import CaboCha, re
+#cabocha = CaboCha.Parser('--charset=UTF8')
+#sent = u"太郎はこの本を二郎を見た女性に渡した。".encode('utf-8')
+#print(cabocha.perseToString(sent))
+#tree = cabocha.parse(sent)
+#print(tree.toString(CaboCha.FORMAT_LATTICE))
+#def cabocha2depgraph(t):
+#    dg = DependencyGraph()
+#    i = 0
+#    for line in t.splitlines():
+#        if line.startswith("*"):
+#            # start of bunsetsu
+#            cells = line.strip().split(" ", 3)
+#            m = re.match(r"([\-0-9]*)([ADIP])", cells[2])
+#            node = dg.nodelist[i]
+#            node.update(
+#              {'address': i,
+#               'rel': m.group(2), # dep_type
+#               'word': [],
+#               'tag': []
+#              })
+#            dep_parent = int(m.group(1))
+#            while len(dg.nodelist) < i+1 or len(dg.nodelist) < dep_parent+1:
+#                dg.nodelist.append({'word':[], 'deps':[], 'tag':[]})
+#            if dep_parent == -1:
+#                dg.root = node
+#            else:
+#                dg.nodelist[dep_parent]['deps'].append(i)
+#            i += 1
+#        elif not line.startswith("EOS"):
+#            # normal morph
+#            cells = line.strip().split("\t")
+#            morph = (cells[0], tuple(cells[1].split(',')))
+#            dg.nodelist[i-1]['word'].append(morph[0])
+#            dg.nodelist[i-1]['tag'].append(morph[1])
+#    return dg
+#def reset_deps(dg):
+#    for node in dg.nodelist:
+#        node['deps'] = []
+#    dg.root = dg.nodelist[-1]
+#def set_head_form(dg):
+#    for node in dg.nodelist:
+#        tags = node['tag']
+#        num_morphs = len(tags)
+#        # extract bhead and bform
+#        bhead = -1
+#        bform = -1
+#        for i in xrange(num_morphs - 1, -1, -1):
+#            if tags[i][0] == u"記号":
+#                continue
+#            else:
+#                if bform == -1:
+#                    bform = i
+#                if not (tags[i][0] == u"" or (tags[i][0] == u"" and tags[i][1] == u"") or tags[i][0] == ""):
+#                    if bhead == -1:
+#                        bhead = i
+#        node['bhead'] = bhead
+#        node['bform'] = bform
+#NEXT_NODE = 1
+#NEXT_VERB_NODE = 2
+#NEXT_NOUN_NODE = 3
+#def get_dep_type(node):
+#    bform_tag = node['tag'][node['bform']]
+#    if bform_tag[0] == u"助詞" and bform_tag[1] == u"格助詞":
+#        return NEXT_VERB_NODE
+#    elif bform_tag[0] == u"助動詞" and bform_tag[1] == u"夕":
+#        return NEXT_NOUN_NODE
+#    else:
+#        return NEXT_NODE
+#def analyze_dependency(dg):
+#    num_nodes = len(dg.nodelist)
+#    for i in xrange(num_nodes-1, 0, -1):
+#        node = dg.nodelist[i]
+#        if i == num_nodes-1:
+#            #last node->to_node=0
+#            to_node = 0
+#        elif i == num_nodes-2:
+#            #one from the last node -> to_node = num_nodes -1
+#            to_node = num_nodes -1
+#        else:
+#            #other nodes
+#            dep_type = get_dep_type(node)
+#            if dep_type == NEXT_NODE:
+#                to_node = i+1
+#            elif dep_type == NEXT_VERB_NODE or dep_type == NEXT_NOUN_NODE:
+#                for j in xrange(i+1, num_nodes):
+#                    node_j = dg.nodelist[j]
+#                    node_j_headtag = node_j['tag'][node_j['bhead']]
+#                    if (node_j['closed'] == False and (dep_type == NEXT_VERB_NODE and node_j_headtag[0] == u"動詞") or 
+#                                                      (dep_type == NEXT_NOUN_NODE and node_j_headtag[0] == u"名詞") and node_j_headtag[1] != u"形容動詞語幹"):
+#                                                      to_node = j
+#                                                      break
+#                                                      node['head'] = to_node
+#                                                      dg.nodelist[to_node]['deps'].append(i)
+#                                                      for j in xrange(i+1, to_node):
+#                                                          dg.nodelist[j]['closed'] = True
+#
+#cabocha_result = u"""*0 7D 0/1 0.000000
+#太郎 名詞,固有名詞,人名,名,*,*,太郎,タロウ,タロー B-PERSON
+#は 助詞,  係助詞,  *,   *, *,*,は,ハ,ワ        0
+#* 1 2D 0/0 1.468291
+#この 連体詞,   *,   *,   *, *,*,この,コノ,コノ 0
+#* 2 4D 0/1 0.742535
+#本 名詞,一般,*,*,*,*,本,ホン,ホン              0
+#を 助詞,格助詞,一般,*,*,*,を,ヲ,ヲ             0
+#* 3 4D 1/2 1.892480
+#二 名詞,数,*,*,*,*,二,二,二                    0
+#郎 名詞,一般,*,*,*,*,郎,ロウ,ロー              0
+#を 助詞,格助詞,一般,*,*,*,を,ヲ,ヲ             0
+#* 4 6D 0/1 0.702689
+#見 動詞,自立,*,*,一段,連用形,見る,ミ,ミ        0
+#た 助動詞,*,*,*,特殊・夕,基本形,た,タ,タ       0
+#* 5 6D 0/1 1.193842
+#きれい 名詞,形容動詞語幹,*,*,*,*,きれい,キレイ,キレイ   0
+#な 助動詞,*,*,*,特殊・ダ,体言接続,だ,ナ,ナ     0
+#* 6 7D 0/1 0.000000
+#女性 名詞,一般,*,*,*,*,女性,ジョセイ,ジョセイ  0
+#に 助詞,格助詞,一般,*,*,*,に,二,二             0
+#* 7 -1D 0/1 0.000000
+#渡し 動詞,自立,*,*,五段・サ行,連用形,渡す,ワタシ,ワタシ  0
+#た 助動詞,*,*,*,特殊・タ,基本形,た,タ,タ       0
+#。 記号,句点,*,*,*,*,。,。,。                  0
+#EOS
+#"""
+#dg = cabocha2depgraph(cabocha_result)
+#reset_deps(dg)
+#set_head_form(dg)
+#def _node_map(n):
+#    node['word'] = '/'.join(node['word']).encode('utf-8')
+#    return node
+#analyze_handrules(dg)
+#print(str(dg.tree()).decode('utf-8'))
 
-#P507~
-cabocha_result = u"""*0 7D 0/1 0.000000
-"""
+#import nltk
+#def extract_case_frame(dg):
+#    res = []
+#    for node in dg.nodelist:
+#        if is_yogen(node):
+#            for case_cand in [dg.nodelist[i] for i in node['deps']]:
+#                if is_case(case_cand):
+#                    res.append([yogen, case_cand])
+#    return res
+#def is_yogen(node):
+#    bhead_tag = node['tag'][node['bhead']]
+#    bform_tag = node['tag'][node['bform']]
+#    if bhead_tag[0] ==u"動詞":
+#        return True
+#    elif bhead_tag[0] ==u"形容詞":
+#        return True
+#    elif bhead_tag[0] ==u"名詞" and bform_tag[0] ==u"助動詞":
+#        return True
+#    else:
+#        return False
+#def is_case(node):
+#    bhead_tag = node['tag'][node['bhead']]
+#    bform_tag = node['tag'][node['bform']]
+#    bform_surface = bform_tag[-1]
+#    if (bform_tag[0] == u"助詞" and bform_tag[1] == u"格助詞" and (bform_surface in set([u"ガ", u"ヲ", u"二", u"ト", u"デ", u"カラ", u"ヨリ", u"ヘ", u"マデ"]))):
+#        return True
+#    elif (bform_tag[0] == u"名詞" and bform_tag[0:2] == [u"名詞",u"接尾"]):
+#        return True
+#    else:
+#        return False
